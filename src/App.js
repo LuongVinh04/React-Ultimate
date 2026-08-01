5e276256058751149cb805b02e742a2cd30ffc17
LuongVinh <luciangold193@gmail.com>
2026-08-01 13:57:00 +0700

    commit buoi 2

--- src/App.js
+++ src/App.js
@@ -3,28 +3,48 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import MyComponent from "./component/MyComponent";
-const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
+import React from "react";
 
-  return (
-    <div>
-      Hello World
-      <MyComponent></MyComponent>
-    </div>
+class App extends React.Component {
+  state = {
+    name: "Luong Vinh",
+    address: "Ha Noi",
+    age: 22,
+  };
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
-};
+  //jsx
+  render() {
+    return (
+      <div>
+        Hello World My name is {this.state.name}
+        <MyComponent></MyComponent>
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   const count = useSelector((state) => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div>
+//       Hello World
+//       <MyComponent></MyComponent>
+//     </div>
+
+// <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       Edit <code>src/App.js</code> and save to reload.
+//     </p>
+//     <div>Count = {count}</div>
+//     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//   </header>
+// </div>
+// );
+// };
 
 export default App;
