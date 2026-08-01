e23afa0310222a6d06ba7dcf520f225ab8af7a21
LuongVinh <luciangold193@gmail.com>
2026-08-01 13:40:19 +0700

    Commit lan dau

---
+++ src/component/MyComponent.js
@@ -0,0 +1,12 @@
+//class component
+//function component
+
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return <div>My fi rst component</div>;
+  }
+}
+
+export default MyComponent;
