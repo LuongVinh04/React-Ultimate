3fc0162a1a9a1f8ff888e707e09187999e61246c
LuongVinh <luciangold193@gmail.com>
2026-08-05 13:55:11 +0700

    Use effect

--- src/component/DisplayInfo.js
+++ src/component/DisplayInfo.js
@@ -1,42 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import UserInfo from "./AddUser";
 import "./DisplayInfo.scss";
 import logo from "./../logo.svg";
 
 //stateless vs stateful
-// class DisplayInfo extends React.Component {
-//   render() {
-//     console.log("call render");
-//     //destructuring array/object
-//     const { listUsers } = this.props;
-//     //props => viet tat properties (dung de truyen du lieu tu component cha sang component con)
-
-//     //template + logic js
-//     return (
-//       <div className="display-info-container">
-//         {true && (
-//           <div>
-//             {listUsers.map((user) => {
-//               return (
-//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                   <div>My name is {user.name}</div>
-//                   <div>My age is {user.age}</div>
-//                   <div>
-//                     <button
-//                       onClick={() => this.props.handleDeleteUser(user.id)}
-//                     >
-//                       Delete
-//                     </button>
-//                   </div>
-//                 </div>
-//               );
-//             })}
-//           </div>
-//         )}
-//       </div>
-//     );
-//   }
-// }
 
 const DisplayInfo = (props) => {
   const { listUsers } = props; //object
@@ -47,11 +14,15 @@ const DisplayInfo = (props) => {
     setShowHideListUser(!isShowHideListUser);
   };
 
-  //destructuring array/object
-
-  //props => viet tat properties (dung de truyen du lieu tu component cha sang component con)
-
-  //template + logic js
+  useEffect(() => {
+    if (listUsers.length === 0) {
+      alert("abc");
+    }
+    setTimeout(() => {
+      document.title = "LuongVinh";
+    }, 3000);
+    console.log("Call me render ");
+  }, [listUsers]);
   return (
     <div className="display-info-container">
       <div>
