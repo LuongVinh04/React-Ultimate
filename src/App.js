63c98fca4512447c7c1bce5ea768b07af0f5dc55
LuongVinh <luciangold193@gmail.com>
2026-08-06 09:05:13 +0700

    Setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./component/MyComponent";
-import React from "react";
 
 const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
-
   return (
     <div className="app-container">
-      Hello World
-      <MyComponent></MyComponent>
+      hello world
+      <button class="btn btn-primary">Test</button>
     </div>
   );
 };
