fe0ef30a27bfd42cb602b301796cd494e547d97f
LuongVinh <luciangold193@gmail.com>
2026-08-18 09:44:32 +0700

    State Modal

--- src/component/Admin/Content/ManageUser.js
+++ src/component/Admin/Content/ManageUser.js
@@ -1,14 +1,16 @@
 import ModalCreateUser from "./ModalCreateUser";
+import "./ManageUser.scss";
 
 const ManageUser = (props) => {
   return (
     <div className="manage-user-container">
       <div className="title">Manage User</div>
       <div className="users-content">
-        <button>Add new user</button>
-      </div>
-      <div>
-        Table Users
+        <div>
+          <button>Add new user</button>
+        </div>
+
+        <div>Table Users</div>
         <ModalCreateUser />
       </div>
     </div>
